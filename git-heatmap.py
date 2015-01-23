57a366a65c59e609ec3de9a610c98e18278fd181
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2015-01-23 09:37:32 +0100

    Handling subprocesses and arguments

--- git-heatmap.py
+++ git-heatmap.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python3
 
+import argparse
 import datetime
 import subprocess
+import os
 import sys
 
+parser = argparse.ArgumentParser(description="Create heatmaps of git commits")
+parser.add_argument("--author",  help="Author whose git commits are to be counted", type=str)
+parser.add_argument("directory", help="git directory to use", metavar="DIR")
+
+arguments = parser.parse_args()
+
+directory = os.path.join(arguments.directory, ".git")
+author    = arguments.author or ""
+
+commits   = subprocess.check_output( ["git", "--git-dir=%s" % directory,
+                                             "log",
+                                             "--pretty=format:%ct",
+                                             "--author=%s" % author ] )
 counts = [ [0]*24 for _ in range(7) ]
 
-with open(sys.argv[1]) as f:
-    for line in f:
-        d   = datetime.datetime.fromtimestamp(int(line))
-        row = d.weekday()
-        col = d.hour
+for commit in commits.decode().split():
+    d   = datetime.datetime.fromtimestamp(int(commit))
+    row = d.weekday()
+    col = d.hour
 
-        counts[row][col] += 1
+    counts[row][col] += 1
 
 for row in range(7):
     for col in range(24):
